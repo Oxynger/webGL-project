02996829e0052c959eb258ce1f7db75ca862d022
oxynger <oxynger@bk.ru>
2018-10-12 15:17:18 +0300

    Add my main project

---
+++ js/backup.js
@@ -0,0 +1,394 @@
+"use strict";
+
+function main() {
+    // Get A WebGL context
+    var canvas = document.getElementById("mainWindow");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    var gl = canvas.getContext("webgl");
+    if (!gl) {
+        return;
+    }
+
+    // Use our boilerplate utils to compile the shaders and link into a program
+    var program = webglUtils.createProgramFromScripts(gl, ["3d-vertex-shader", "3d-fragment-shader"]);
+
+    // look up where the vertex data needs to go.
+    var positionLocation = gl.getAttribLocation(program, "a_position");
+    var colorLocation = gl.getAttribLocation(program, "a_color");
+
+    // look up uniform locations
+    var matrixLocation = gl.getUniformLocation(program, "u_matrix");
+
+    // Create a buffer and put three 2d clip space points in it
+    var positionBuffer = gl.createBuffer();
+    var colorBuffer = gl.createBuffer();
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+
+    setColor(gl);
+
+    // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    setGeometry(gl);
+
+    var translation = [0, 0, -360];
+    var rotation = [degreesToRadians(190), degreesToRadians(40), degreesToRadians(320)];
+    var scale = [1, 1, 1];
+    var fieldOfViewRadians = degreesToRadians(60);
+    var rotationSpeed = 1.2;
+
+
+    var before = 0;
+    requestAnimationFrame(drawScene);
+
+    // Draw the scene.
+    function drawScene(now) {
+        // time in second
+        now *= 0.001;
+
+        var deleyTime = now - before;
+
+        before = now;
+
+        // Every frame increase the rotation a little.
+        rotation[1] += rotationSpeed * deleyTime;  // don't do this
+
+        webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+        // Tell WebGL how to convert from clip space to pixels
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+        // Clear the canvas AND the depth buffer.
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        // Turn on culling. By default backfacing triangles
+        // will be culled.
+        gl.enable(gl.CULL_FACE);
+
+        // Enable the depth buffer
+        gl.enable(gl.DEPTH_TEST);
+
+        // Tell it to use our program (pair of shaders)
+        gl.useProgram(program);
+
+        // Turn on the position attribute
+        gl.enableVertexAttribArray(positionLocation);
+
+        // Bind the position buffer.
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+        // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+        var size = 3;          // 3 components per iteration
+        var type = gl.FLOAT;   // the data is 32bit floats
+        var normalize = false; // don't normalize the data
+        var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+        var offset = 0;        // start at the beginning of the buffer
+        gl.vertexAttribPointer(
+            positionLocation, size, type, normalize, stride, offset)
+
+        // Turn on the color attribute
+        gl.enableVertexAttribArray(colorLocation);
+
+        // Bind the color buffer.
+        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+
+        // Tell the attribute how to get data out of colorBuffer (ARRAY_BUFFER)
+        var size = 3;                 // 3 components per iteration
+        var type = gl.UNSIGNED_BYTE;  // the data is 8bit unsigned values
+        var normalize = true;         // normalize the data (convert from 0-255 to 0-1)
+        var stride = 0;               // 0 = move forward size * sizeof(type) each iteration to get the next position
+        var offset = 0;               // start at the beginning of the buffer
+        gl.vertexAttribPointer(
+            colorLocation, size, type, normalize, stride, offset)
+
+        // Compute the matrices
+        var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+        var matrix = m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
+        matrix = m4.translate(matrix, translation[0], translation[1], translation[2]);
+        matrix = m4.xRotate(matrix, rotation[0]);
+        matrix = m4.yRotate(matrix, rotation[1]);
+        matrix = m4.zRotate(matrix, rotation[2]);
+        matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
+
+        // Set the matrix.
+        gl.uniformMatrix4fv(matrixLocation, false, matrix);
+
+        // Draw the geometry.
+        var primitiveType = gl.TRIANGLES;
+        var offset = 0;
+        var count = 16 * 6;
+        gl.drawArrays(primitiveType, offset, count);
+
+        // Call drawScene again next frame
+        requestAnimationFrame(drawScene);
+    }
+
+}
+
+var m4 = {
+    translation: function (tx, ty, tz) {
+        return [
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            tx, ty, tz, 1,
+        ];
+    },
+
+    xRotation: function (angleInRadians) {
+        var c = Math.cos(angleInRadians);
+        var s = Math.sin(angleInRadians);
+
+        return [
+            1, 0, 0, 0,
+            0, c, s, 0,
+            0, -s, c, 0,
+            0, 0, 0, 1,
+        ];
+    },
+
+    yRotation: function (angleInRadians) {
+        var c = Math.cos(angleInRadians);
+        var s = Math.sin(angleInRadians);
+
+        return [
+            c, 0, -s, 0,
+            0, 1, 0, 0,
+            s, 0, c, 0,
+            0, 0, 0, 1,
+        ];
+    },
+
+    zRotation: function (angleInRadians) {
+        var c = Math.cos(angleInRadians);
+        var s = Math.sin(angleInRadians);
+
+        return [
+            c, s, 0, 0,
+            -s, c, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1,
+        ];
+    },
+
+    scaling: function (sx, sy, sz) {
+        return [
+            sx, 0, 0, 0,
+            0, sy, 0, 0,
+            0, 0, sz, 0,
+            0, 0, 0, 1,
+        ];
+    },
+
+    translate: function (m, tx, ty, tz) {
+        return m4.multiply(m, m4.translation(tx, ty, tz));
+    },
+
+    xRotate: function (m, angleInRadians) {
+        return m4.multiply(m, m4.xRotation(angleInRadians));
+    },
+
+    yRotate: function (m, angleInRadians) {
+        return m4.multiply(m, m4.yRotation(angleInRadians));
+    },
+
+    zRotate: function (m, angleInRadians) {
+        return m4.multiply(m, m4.zRotation(angleInRadians));
+    },
+
+    scale: function (m, sx, sy, sz) {
+        return m4.multiply(m, m4.scaling(sx, sy, sz));
+    },
+
+    perspective: function (fieldOfViewInRadians, aspect, near, far) {
+        var f = Math.tan(Math.PI * 0.5 - 0.5 * fieldOfViewInRadians);
+        var rangeInv = 1.0 / (near - far);
+
+        return [
+            f / aspect, 0, 0, 0,
+            0, f, 0, 0,
+            0, 0, (near + far) * rangeInv, -1,
+            0, 0, near * far * rangeInv * 2, 0
+        ];
+    },
+
+    multiply: function (aMatrix, bMatrix) {
+        var productMatrix = [
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+        ];
+
+        for (let row = 0; row < 4; row++) {
+            for (let col = 0; col < 4; col++) {
+                for (let inner = 0; inner < 4; inner++) {
+                    productMatrix[row * 4 + col] += bMatrix[row * 4 + inner] * aMatrix[inner * 4 + col];
+                }
+            }
+        }
+        return productMatrix;
+    },
+
+    vectorMultiply: function (v, m) {
+        var dst = [];
+        for (var i = 0; i < 4; ++i) {
+            dst[i] = 0.0;
+            for (var j = 0; j < 4; ++j)
+                dst[i] += v[j] * m[j * 4 + i];
+        }
+        return dst;
+    }
+};
+
+function degreesToRadians(degrees) {
+    return degrees * Math.PI / 180;
+}
+
+function randomInt(range) {
+    return Math.floor(Math.random() * range);
+}
+
+function setColor(gl) {
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Uint8Array([
+            // left column front
+            200, 70, 120,
+            200, 70, 120,
+            200, 70, 120,
+            200, 70, 120,
+            200, 70, 120,
+            200, 70, 120,
+
+            // top rung front
+            200, 70, 120,
+            200, 70, 120,
+            200, 70, 120,
+            200, 70, 120,
+            200, 70, 120,
+            200, 70, 120,
+
+            // middle rung front
+            200, 70, 120,
+            200, 70, 120,
+            200, 70, 120,
+            200, 70, 120,
+            200, 70, 120,
+            200, 70, 120,
+
+            // left column back
+            80, 70, 200,
+            80, 70, 200,
+            80, 70, 200,
+            80, 70, 200,
+            80, 70, 200,
+            80, 70, 200,
+
+            // top rung back
+            80, 70, 200,
+            80, 70, 200,
+            80, 70, 200,
+            80, 70, 200,
+            80, 70, 200,
+            80, 70, 200,
+
+            // middle rung back
+            80, 70, 200,
+            80, 70, 200,
+            80, 70, 200,
+            80, 70, 200,
+            80, 70, 200,
+            80, 70, 200,
+
+            // top
+            70, 200, 210,
+            70, 200, 210,
+            70, 200, 210,
+            70, 200, 210,
+            70, 200, 210,
+            70, 200, 210,
+
+            // top rung right
+            200, 200, 70,
+            200, 200, 70,
+            200, 200, 70,
+            200, 200, 70,
+            200, 200, 70,
+            200, 200, 70,
+
+            // under top rung
+            210, 100, 70,
+            210, 100, 70,
+            210, 100, 70,
+            210, 100, 70,
+            210, 100, 70,
+            210, 100, 70,
+
+            // between top rung and middle
+            210, 160, 70,
+            210, 160, 70,
+            210, 160, 70,
+            210, 160, 70,
+            210, 160, 70,
+            210, 160, 70,
+
+            // top of middle rung
+            70, 180, 210,
+            70, 180, 210,
+            70, 180, 210,
+            70, 180, 210,
+            70, 180, 210,
+            70, 180, 210,
+
+            // right of middle rung
+            100, 70, 210,
+            100, 70, 210,
+            100, 70, 210,
+            100, 70, 210,
+            100, 70, 210,
+            100, 70, 210,
+
+            // bottom of middle rung.
+            76, 210, 100,
+            76, 210, 100,
+            76, 210, 100,
+            76, 210, 100,
+            76, 210, 100,
+            76, 210, 100,
+
+            // right of bottom
+            140, 210, 80,
+            140, 210, 80,
+            140, 210, 80,
+            140, 210, 80,
+            140, 210, 80,
+            140, 210, 80,
+
+            // bottom
+            90, 130, 110,
+            90, 130, 110,
+            90, 130, 110,
+            90, 130, 110,
+            90, 130, 110,
+            90, 130, 110,
+
+            // left side
+            160, 160, 220,
+            160, 160, 220,
+            160, 160, 220,
+            160, 160, 220,
+            160, 160, 220,
+            160, 160, 220,
+        ]), gl.STATIC_DRAW);
+}
+
+function setGeometry(gl) {
+
+
+    gl.bufferData(gl.ARRAY_BUFFER, position, gl.STATIC_DRAW);
+}
+
+main();
---
+++ js/main.js
@@ -0,0 +1,294 @@
+"use strict";
+
+function main() {
+    // Get A WebGL context
+    var canvas = document.getElementById("mainWindow");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    var gl = canvas.getContext("webgl");
+    gl = WebGLDebugUtils.makeDebugContext(gl, undefined, validateNoneOfTheArgsAreUndefined);
+
+    if (!gl) {
+        return;
+    }
+
+    let positionFInfo = new MakePositionInfo(
+        [-50, 75, -15],
+        [0, 0, 0],
+        [1, 1, 1],
+    );
+
+    let polygonPositionInfo = new MakePositionInfo(
+        [150, 75, -15],
+        [0, 0, 0],
+        [1, 1, 1],
+    );
+
+    let cameraInfo = new MakeCameraInfo(
+        [0, 150, 300],
+        [0, 0, 0],
+        60,
+    );
+
+    const figures = {};
+
+    figures["letter"] = new MakeObject(
+        "3d-vertex-shader", "3d-fragment-shader", gl, primitive.FsFigure, positionFInfo
+    );
+
+    figures["polygon"] = new MakeObject(
+        "3d-vertex-shader", "3d-fragment-shader", gl, primitive.polygon, polygonPositionInfo
+    );
+
+    window.scene = new MakeScene(gl, figures, cameraInfo);
+
+    // Draw the scene.
+    function drawScene(now) {
+
+        scene.draw(now);
+
+        // Call drawScene again next frame
+        requestAnimationFrame(drawScene);
+    }
+
+    requestAnimationFrame(drawScene);
+
+}
+
+function MakeCameraInfo(cameraPosition, target, degreesFieldOfView) {
+    this.cameraPosition = cameraPosition;
+    this.target = target;
+    this.RadiansfieldOfView = degreesToRadians(degreesFieldOfView);
+
+    let up = [0, 1, 0];
+    let cameraMatrix = m4.lookAt(cameraPosition, target, up);
+
+    // Make a view matrix from the camera matrix.
+    let viewMatrix = m4.inverse(cameraMatrix);
+
+    this.getMatrix = () => {
+        cameraMatrix = m4.lookAt(cameraPosition, target, up);
+
+        // Make a view matrix from the camera matrix.
+        viewMatrix = m4.inverse(cameraMatrix);
+
+        return viewMatrix;
+    }
+}
+
+function MakeScene(gl, figures, cameraInfo) {
+    this.figures = figures;
+    this.cameraInfo = cameraInfo;
+    let viewProjectionMatrix = null;
+
+    let before = 0;
+
+    this.draw = function (now) {
+        // time in second
+        now *= 0.001;
+
+        let deleyTime = now - before;
+
+        before = now;
+
+        // Every frame increase the rotation a little.
+        this.figures["letter"].rotate(deleyTime);
+
+        // Tell WebGL how to convert from clip space to pixels
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+        // Clear the canvas AND the depth buffer.
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        gl.enable(gl.CULL_FACE);
+        gl.enable(gl.DEPTH_TEST);
+
+        let aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+        var projectionMatrix =
+            m4.perspective(this.cameraInfo.RadiansfieldOfView, aspect, 1, 2000);
+
+        viewProjectionMatrix = m4.multiply(projectionMatrix, cameraInfo.getMatrix());
+
+        for (const key in figures) {
+            if (figures.hasOwnProperty(key)) {
+                figures[key].matrix = viewProjectionMatrix;
+                figures[key].draw();
+            }
+        }
+    }
+}
+
+function MakePositionInfo(translation, degreesRotation, scale) {
+    this.translation = translation;
+    this.scale = scale;
+    this.rotation = [];
+
+    degreesRotation.forEach((currentDegrees, index) => {
+        this.rotation[index] = degreesToRadians(currentDegrees);
+    });
+
+    this.getMatrix = function () {
+        let matrix = m4.identity;
+        matrix = m4.translate(matrix, this.translation[0], this.translation[1], this.translation[2]);
+        matrix = m4.xRotate(matrix, this.rotation[0]);
+        matrix = m4.yRotate(matrix, this.rotation[1]);
+        matrix = m4.zRotate(matrix, this.rotation[2]);
+        matrix = m4.scale(matrix, this.scale[0], this.scale[1], this.scale[2]);
+
+        return matrix;
+    }
+}
+
+function MakeObject(vertexShaderId, fragmentShaderId, gl, GeometryFigure, position) {
+    this.program = webglUtils.createProgramFromScripts(gl, [vertexShaderId, fragmentShaderId]);
+    this.matrix = m4.identity;
+    this.worldRotation = 0;
+
+    let color = [Math.random(), Math.random(), Math.random(), 1]; // rgba
+
+    // look up atrribute locations
+    this.positionLocation = gl.getAttribLocation(this.program, "a_position");
+    this.normalLocation = gl.getAttribLocation(this.program, "a_normal");
+
+    // look up uniform locations
+    this.worldViewProjectionLocation =
+        gl.getUniformLocation(this.program, "u_worldViewProjection");
+    this.worldInverseTransposeLocation =
+        gl.getUniformLocation(this.program, "u_worldInverseTranspose");
+
+    this.colorLocation = gl.getUniformLocation(this.program, "u_color");
+
+    this.reverseLightDirectionLocation =
+        gl.getUniformLocation(this.program, "u_reverseLightDirection");
+
+    // Create a buffer and put three 2d clip space points in it
+    this.positionBuffer = gl.createBuffer();
+    this.position = position;
+
+    setObject = setObject.bind(this);
+    setObject();
+
+    this.normalBuffer = gl.createBuffer();
+    setNormal = setNormal.bind(this);
+    setNormal();
+
+    this.rotationSpeed = -0.5;
+    this.count = GeometryFigure.Primitive.length / 3;
+
+    this.rotate = (delay) => {
+        this.worldRotation += delay * this.rotationSpeed;
+    }
+
+    this.draw = () => {
+        // Tell it to use our programForF (pair of shaders)
+        gl.useProgram(this.program);
+
+        // Turn on the position attribute
+        gl.enableVertexAttribArray(this.positionLocation);
+
+        // Bind the position buffer.
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
+
+        // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+        var size = 3;                 // 3 components per iteration
+        var type = gl.FLOAT;          // the data is 32bit floats
+        var normalize = false;        // don't normalize the data
+        var stride = 0;               // 0 = move forward size * sizeof(type) each iteration to get the next position
+        var offset = 0;         // start at the beginning of the buffer
+        gl.vertexAttribPointer(
+            this.positionLocation, size, type, normalize, stride, offset)
+
+        // Turn on the normal attribute
+        gl.enableVertexAttribArray(this.normalLocation);
+
+        // Bind the normal buffer.
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.normalBuffer);
+
+        // Tell the attribute how to get data out of normalBuffer (ARRAY_BUFFER)
+        var size = 3;          // 3 components per iteration
+        var type = gl.FLOAT;   // the data is 32bit floating point values
+        var normalize = false; // normalize the data (convert from 0-255 to 0-1)
+        var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+        var offset = 0;        // start at the beginning of the buffer
+        gl.vertexAttribPointer(
+            this.normalLocation, size, type, normalize, stride, offset)
+
+        this.matrix = m4.multiply(this.matrix, position.getMatrix())
+
+        // Draw a F at the origin
+        let worldMatrix = m4.yRotation(this.worldRotation);
+
+        // Multiply the matrices.
+        let worldViewProjectionMatrix = m4.multiply(this.matrix, worldMatrix);
+        let worldInverseMatrix = m4.inverse(worldMatrix);
+        let worldInverseTransposeMatrix = m4.transpose(worldInverseMatrix);
+
+        // Set the matrix.
+        gl.uniformMatrix4fv(
+            this.worldViewProjectionLocation, false,
+            worldViewProjectionMatrix);
+        gl.uniformMatrix4fv(this.worldInverseTransposeLocation, false, worldInverseTransposeMatrix);
+
+        // Set the color to use
+        gl.uniform4fv(this.colorLocation, color);
+
+        // set the light direction.
+        gl.uniform3fv(this.reverseLightDirectionLocation, m4.normalize([0.5, 0.7, 1]));
+
+        // Draw the geometry.
+        let primitiveType = gl.TRIANGLES;
+        let arrayOffset = 0;
+        gl.drawArrays(primitiveType, arrayOffset, this.count);
+    }
+
+    function setNormal() {
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.normalBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, GeometryFigure.normals, gl.STATIC_DRAW);
+
+    }
+
+    function setObject() {
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
+
+        let reversalMatrix = m4.xRotation(Math.PI)
+
+        for (var ii = 0; ii < GeometryFigure.Primitive.length; ii += 3) {
+            var vector = m4.transformPoint(reversalMatrix, [GeometryFigure.Primitive[ii + 0], GeometryFigure.Primitive[ii + 1], GeometryFigure.Primitive[ii + 2], 1]);
+            GeometryFigure.Primitive[ii + 0] = vector[0];
+            GeometryFigure.Primitive[ii + 1] = vector[1];
+            GeometryFigure.Primitive[ii + 2] = vector[2];
+        }
+
+        gl.bufferData(gl.ARRAY_BUFFER, GeometryFigure.Primitive, gl.STATIC_DRAW);
+    }
+}
+
+function degreesToRadians(degrees) {
+    return degrees * Math.PI / 180;
+}
+
+function randomInt(range) {
+    return Math.floor(Math.random() * range);
+}
+
+function logGLCall(functionName, args) {
+    console.log("gl." + functionName + "(" +
+        WebGLDebugUtils.glFunctionArgsToString(functionName, args) + ")");
+}
+
+function validateNoneOfTheArgsAreUndefined(functionName, args) {
+    for (var ii = 0; ii < args.length; ++ii) {
+        if (args[ii] === undefined) {
+            console.error("undefined passed to gl." + functionName + "(" +
+                WebGLDebugUtils.glFunctionArgsToString(functionName, args) + ")");
+        }
+    }
+}
+
+function throwOnGLError(err, funcName, args) {
+    throw WebGLDebugUtils.glEnumToString(err) + " was caused by call to: " + funcName;
+}
+
+
+main();
